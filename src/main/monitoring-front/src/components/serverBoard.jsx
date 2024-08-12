901c0b302cb24d7f6ab70a28aa74b58bcd22024a
elcrity <elcrity@naver.com>
2024-08-12 09:06:14 +0900

    front add

--- src/main/monitoring-front/src/components/serverBoard.jsx
+++ src/main/monitoring-front/src/components/serverBoard.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../css/serverboard.css';
 
-const ServerBoard = ({ data, onDiskClick, onDeleteClick  }) => {
-    const {
-        serverId, serverOs, serverHostname,
-        memoryTotal, purpose, serverIp,
-        cpuUsage, memoryUsage, diskUsage, disks
-    } = data;
+const ServerBoard = ({ data, onDiskClick }) => {
+  const {
+    serverId, serverOs, serverHostname,
+    memoryTotal, purpose, serverIp,
+    cpuUsage, memoryUsage, diskUsage, disks
+  } = data;
 
-    return (
-        <div id='serverTable' key={serverId} onClick={() => onDiskClick(disks, serverId)}>
-            <div>
-                <div>{serverHostname}</div>
-                <div>{serverIp}</div>
-            </div>
-            <div id="cpuUsage">{cpuUsage}</div>
-            <div id="memoryUsage">{memoryUsage}</div>
-            <div id="diskUsage">{diskUsage}</div>
-        </div>
-    );
+  const [statusIndicatorColor, setStatusIndicatorColor] = useState('transparent');
+
+  useEffect(() => {
+    const maxUsage = Math.max(cpuUsage || 0, memoryUsage || 0, diskUsage || 0);
+
+    if (maxUsage >= 90) {
+      setStatusIndicatorColor('#ff0000'); // 빨간색
+    } else if (maxUsage >= 70) {
+      setStatusIndicatorColor('#ffa500'); // 오렌지색
+    } else {
+      setStatusIndicatorColor('transparent'); // 초기 상태
+    }
+  }, [cpuUsage, memoryUsage, diskUsage]);
+
+  return (
+     <div id='serverTable' key={serverId} onClick={() => onDiskClick(disks, serverId)}>
+       <div id='serverTable-header'>
+         <div>{serverHostname}</div>
+         <div id='statusIndicator' style={{ backgroundColor: statusIndicatorColor }}></div>
+         <div>{serverIp}</div>
+       </div>
+       {cpuUsage > 0 && (
+          <div id='cpuUsageContainer'>
+            <div id="cpuUsage" style={{ width: `${cpuUsage || 0}%` }}></div>
+            <div>{cpuUsage}%</div>
+          </div>
+       )}
+       {memoryUsage > 0 && (
+          <div id='memoryUsageContainer'>
+            <div id="memoryUsage" style={{ width: `${memoryUsage || 0}%` }}></div>
+            <div>{memoryUsage}%</div>
+          </div>
+       )}
+       {diskUsage > 0 && (
+          <div id='diskUsageContainer'>
+            <div id="diskUsage" style={{ width: `${diskUsage || 0}%` }}></div>
+            <div>{diskUsage}%</div>
+          </div>
+       )}
+     </div>
+  );
 };
 
-export default ServerBoard
+export default ServerBoard;
--- src/main/monitoring-front/src/pages/history.jsx
+++ src/main/monitoring-front/src/pages/history.jsx
@@ -1,122 +1,228 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { call } from '../util/call';
+import '../css/history.css';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+// Chart.js 모듈 등록
+ChartJS.register(PointElement, LineElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
 
 const History = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { id } = location.state || {}; // params 객체에서 id를 추출합니다.
-    const [server, setServer] = useState([]);
-    const [metrics, setMetrics] = useState([]);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(true);
-    
-    
-    useEffect(() => {
-        if (!id) {
-            navigate('/'); // id가 없으면 홈으로 리디렉션
-            return;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { id } = location.state || {}; // params 객체에서 id를 추출합니다.
+  const [server, setServer] = useState({});
+  const [metrics, setMetrics] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'CPU Usage',
+        data: [],
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        fill: true,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    if (!id) {
+      navigate('/'); // id가 없으면 홈으로 리디렉션
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        console.log("id: ", id);
+        const data = await call(`/api/history/${id.current}`, 'POST');
+        const logs = await call(`/log/history/${id.current}`, 'POST');
+        setServer(data);
+        setMetrics(logs);
+        setLoading(false);
+
+        // 컴포넌트가 처음 마운트될 때 CPU Usage 차트로 초기화
+        handleDivClick('CPU');
+      } catch (error) {
+        if (error.status != null) {
+          const errorData = await error.json();
+          setErrorMessage(errorData);
         }
-        const fetchData = async () => {
-            try {
-                console.log("id: " , id);
-                const data = await call(`/api/history/${id.current}`, 'POST');
-                const logs = await call(`/log/history/${id.current}`, 'POST')
-                setServer(data);
-                setMetrics(logs);
-                setLoading(true);
-            } catch (error) {
-                if (error.status != null) {
-                    const errorData = await error.json();
-                    setErrorMessage(errorData);
-                }
-            }finally{
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [id])
-
-    const handleButtonClick = () => {
-        navigate(`/`);
+        setLoading(false);
+      }
     };
-    if (loading) {
-        return <div>데이터를 가져오는 중...</div>; // 로딩 중일 때 표시할 내용
+
+    fetchData();
+  }, [id, navigate]);
+
+  const handleButtonClick = () => {
+    navigate(`/`);
+  };
+
+  const handleDivClick = (type) => {
+    let newData = {
+      labels: metrics.map((log) => new Date(log.createdDate).toLocaleDateString()),
+      datasets: []
+    };
+
+    switch (type) {
+      case 'CPU':
+        newData.datasets.push({
+          label: 'CPU Usage',
+          data: metrics.map((log) => log.cpuUsage),
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          fill: true,
+        });
+        break;
+      case 'Memory':
+        newData.datasets.push({
+          label: 'Memory Usage',
+          data: metrics.map((log) => log.memoryUsage),
+          borderColor: 'rgba(153, 102, 255, 1)',
+          backgroundColor: 'rgba(153, 102, 255, 0.2)',
+          fill: true,
+        });
+        break;
+      case 'Disk1':
+        newData.datasets.push({
+          label: 'Disk1 Usage',
+          data: metrics.map((log) => log.diskUsage1),
+          borderColor: 'rgba(255, 159, 64, 1)',
+          backgroundColor: 'rgba(255, 159, 64, 0.2)',
+          fill: true,
+        });
+        break;
+      case 'Disk2':
+        newData.datasets.push({
+          label: 'Disk2 Usage',
+          data: metrics.map((log) => log.diskUsage2),
+          borderColor: 'rgba(255, 99, 132, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          fill: true,
+        });
+        break;
+      case 'Disk3':
+        newData.datasets.push({
+          label: 'Disk3 Usage',
+          data: metrics.map((log) => log.diskUsage3),
+          borderColor: 'rgba(54, 162, 235, 1)',
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          fill: true,
+        });
+        break;
+      case 'Disk4':
+        newData.datasets.push({
+          label: 'Disk4 Usage',
+          data: metrics.map((log) => log.diskUsage4),
+          borderColor: 'rgba(255, 206, 86, 1)',
+          backgroundColor: 'rgba(255, 206, 86, 0.2)',
+          fill: true,
+        });
+        break;
+      default:
+        break;
     }
 
-    if (errorMessage) {
-        return (<div>
-            <p>{errorMessage.message}</p>
-            <p>{errorMessage.status}</p>
-            <p>{errorMessage.name}</p>
-            <p>{errorMessage.code}</p>
-        </div>)
-    } else {
-        return (
-            <div>
-                <button onClick={handleButtonClick} className="btn btn-primary">
-                    이전
-                </button>
-                <h1>서버 히스토리</h1>
+    setChartData(newData);
+  };
 
-                {/* 서버 정보 , /api/history/{serverId}*/}
-                <h2 id="serverInfoDiv">Server Info</h2>
-                <p><strong>Server ID:</strong> {server.serverId || ''}</p>
-                <p><strong>Operating System:</strong> {server.serverOs || ''}</p>
-                <p><strong>Hostname:</strong> {server.serverHostname || ''}</p>
-                <p><strong>Purpose:</strong> {server.purpose || ''}</p>
-                <p><strong>IP Address:</strong> {server.serverIp || ''}</p>
+  if (loading) {
+    return <div>데이터를 가져오는 중...</div>; // 로딩 중일 때 표시할 내용
+  }
 
-                {/* 로그정보 ,/log/history/{serverId} */}
-                <h2>Metric Logs</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Log Index</th>
-                            <th>CPU Usage</th>
-                            <th>Memory Usage</th>
-                            <th>Disk Usage 1</th>
-                            <th>Disk Usage 2</th>
-                            <th>Disk Usage 3</th>
-                            <th>Disk Usage 4</th>
-                            <th>Disk Total 1</th>
-                            <th>Disk Total 2</th>
-                            <th>Disk Total 3</th>
-                            <th>Disk Total 4</th>
-                            <th>Disk Name 1</th>
-                            <th>Disk Name 2</th>
-                            <th>Disk Name 3</th>
-                            <th>Disk Name 4</th>
-                            <th>Created Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {metrics.map((log, index) => (
-                            <tr key={log.logId}>
-                                <td>{index}</td>
-                                <td>{`${log.cpuUsage}%` || ''}</td>
-                                <td>{`${log.memoryUsage}%` || ''}</td>
-                                <td>{log.diskUsage1 != null ? `${log.diskUsage1}%` : ''}</td>
-                                <td>{log.diskUsage2 != null ? `${log.diskUsage2}%` : ''}</td>
-                                <td>{log.diskUsage3 != null ? `${log.diskUsage3}%` : ''}</td>
-                                <td>{log.diskUsage3 != null ? `${log.diskUsage4}%` : ''}</td>
-                                <td>{`${(log.diskTotal1 / 1024).toFixed(2)}GB` || ''}</td>
-                                <td>{log.diskTotal2 || ''}</td>
-                                <td>{log.diskTotal3 || ''}</td>
-                                <td>{log.diskTotal4 || ''}</td>
-                                <td>{log.diskName1 || ''}</td>
-                                <td>{log.diskName2 || ''}</td>
-                                <td>{log.diskName3 || ''}</td>
-                                <td>{log.diskName4 || ''}</td>
-                                <td>{new Date(log.createdDate).toLocaleString('ko-KR') || ''}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+  if (errorMessage) {
+    return (
+       <div>
+         <p>{errorMessage.message}</p>
+         <p>{errorMessage.status}</p>
+         <p>{errorMessage.name}</p>
+         <p>{errorMessage.code}</p>
+       </div>
+    );
+  }
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => `${context.dataset.label}: ${context.raw}%`,
+        },
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Date',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Usage (%)',
+        },
+        beginAtZero: true,
+        max: 100,
+      },
+    },
+  };
+
+  return (
+     <div id='historyContainer'>
+       <h1>서버 히스토리</h1>
+       <div id='menu'>
+         <div id='historyCpu' onClick={() => handleDivClick('CPU')}>
+           <div>CPU</div>
+           <div></div>
+         </div>
+         <div id='historyMemory' onClick={() => handleDivClick('Memory')}>
+           <div>Memory</div>
+           <div></div>
+         </div>
+         <div id='historyDisk1' onClick={() => handleDivClick('Disk1')}>
+           <div>Disk1</div>
+           <div></div>
+         </div>
+         <div id='historyDisk2' onClick={() => handleDivClick('Disk2')}>
+           <div>Disk2</div>
+           <div></div>
+         </div>
+         <div id='historyDisk3' onClick={() => handleDivClick('Disk3')}>
+           <div>Disk3</div>
+           <div></div>
+         </div>
+         <div id='historyDisk4' onClick={() => handleDivClick('Disk4')}>
+           <div>Disk4</div>
+           <div></div>
+         </div>
+         <div>
+           <button onClick={handleButtonClick} className="btn btn-primary">
+             이전
+           </button>
+         </div>
+       </div>
+       <div id='chart'>
+         <h2>CPU Usage Chart</h2>
+         <Line data={chartData} options={chartOptions} />
+       </div>
+     </div>
+  );
 };
 
 export default History;
